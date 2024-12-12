c460665e9ae009d46f0778885fa5da7a4f3e67a8
Carvalima Transportes <botlorien@gmail.com>
2024-12-12 12:06:00 -0400

    Update version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="etl-dataprocess",
-    version="0.1.0",
+    version="0.1.1",
     description='Functions utils to perform data processing',
     author='Ben-Hur P. B. Santos',
     author_email='botlorien@gmail.com',
     long_description=open("README.md", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/botlorien/botweb",  # Link para o repositório
+    url="https://github.com/botlorien/dataprocess",  # Link para o repositório
     packages=find_packages(),  # Especifica que os pacotes estão na pasta src
     #  package_dir={"": "src"},  # Configura o diretório-base como src
     classifiers=[
